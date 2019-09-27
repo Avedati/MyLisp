fefd207d735133c3d1a002ac57da28d7d3e8d387
Abhinav Vedati <52864339+Avedati@users.noreply.github.com>
2019-09-27 10:01:54 -0700

    Update lisp.c

--- src/lisp.c
+++ src/lisp.c
@@ -4,6 +4,18 @@
 #include "../include/symtable.h"
 #include <stdlib.h>
 
+/*
+  struct lisp* lisp_create(struct str_it* str_it)
+  
+  This function returns a pointer to a new lisp struct.
+  The lisp struct is passed to every function in the parser.
+  It will allocate the necessary memory and set the
+  lisp's string iterator to the string iterator provided,
+  and will create a new stack and symbol table.
+  
+  @param str_it A pointer to a str_it struct.
+  @return A pointer to a new lisp struct.
+*/
 struct lisp* lisp_create(struct str_it* str_it) {
 	struct lisp* lisp = malloc(sizeof(struct lisp*));
 	lisp->str_it = str_it;
@@ -12,6 +24,17 @@ struct lisp* lisp_create(struct str_it* str_it) {
 	return lisp;
 }
 
+/*
+  void lisp_destroy(struct lisp* lisp)
+  
+  This function frees all data in the pointer
+  to a lisp struct provided. It will free
+  the string iteratr, the stack, and the
+  symbol table, as well as the lisp struct
+  itself.
+  
+  @param lisp A pointer to a lisp struct.
+*/
 void lisp_destroy(struct lisp* lisp) {
 	str_it_destroy(lisp->str_it);
 	stack_destroy(lisp->stack);
